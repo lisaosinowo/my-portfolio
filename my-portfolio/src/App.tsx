56235bf052260a8fc8a50b494a2b24f8789e8fae
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-19 16:25:29 +0100

    Added the hero desktop picture, profile picture, and the follow button for the header

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
+import DesktopPic from "./media/hillary-black-vFJN8Nde1Wc-unsplash.jpg"
+import LisaCircleImg from "./media/lisa-pink.jpg"
 
 function App() {
 
   return (
     <div className="">
-      <h1 className="text-3xl font-bold text-blue-500 flex justify-center bg-purple-100">
-        Hello World!
-      </h1>
+      <div className="">
+        {/* Hero picture of desktop */}
+        <img src={DesktopPic} alt="Desktop" className="flex w-5/6 h-85 justify-self-center lg:w-[700px]" />
+        {/* Profile pic + follow button */}
+        <div className="flex justify-between justify-self-center items-center mt-4 w-2/3 lg:w-[550px]">
+          <img src={LisaCircleImg} alt="Lisa" className="w-32 h-32 rounded-full border-2" />
+          <button className="bg-[--lilac2] text-white font-bold py-2 px-6 rounded-3xl">Follow</button>
+        </div>
+      </div>
+      
     </div>
   )
 }
