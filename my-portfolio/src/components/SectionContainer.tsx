cc03be1490033e5f11835fa214bdc0004827d67e
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-20 17:40:15 +0100

    Added the SectionContainer component that will contain the different sections content

---
+++ my-portfolio/src/components/SectionContainer.tsx
@@ -0,0 +1,11 @@
+import Feed from "./Feed"
+
+const SectionContainer = () => {
+  return (
+    <div className="border border-[var(--dark-navy-purple3)] border-4 rounded text-white main-container-width cursor-pointer">
+      <Feed />
+    </div>
+  )
+}
+
+export default SectionContainer
