8b656c2675b629ba05ee120e96e2cec5d85d7953
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-20 14:11:14 +0100

    Added the SectionButton components and the ProfileInformation component to the MainContainer component

--- my-portfolio/src/components/MainContainer.tsx
+++ my-portfolio/src/components/MainContainer.tsx
@@ -1,13 +1,23 @@
-import ProfileInformation from './ProfileInformation'
+import ProfileInformation from "./ProfileInformation";
+import SectionButton from "./SectionButton";
 
 const MainContainer = () => {
-  return (
-    <div className='relative flex justify-center'>
-      <div className='border absolute top-[6.5rem] flex main-container-width lg:top-24 md:top-32 sm:top-32'>
-         <ProfileInformation />
-      </div>
-    </div>
-  )
-}
+   return (
+      <div className="">
+         <div className="relative flex justify-center">
+            <div className="border absolute top-[6.5rem] flex main-container-width lg:top-24 md:top-32 sm:top-32">
+               <ProfileInformation />
+            </div>
+         </div>
 
-export default MainContainer
+         <div className="flex main-container-width mt-[16.5rem] sm:mt-[18rem] md:mt-[18rem] lg:mt-[16rem] justify-around">
+            <SectionButton text="Feed" />
+            <SectionButton text="About" />
+            <SectionButton text="Languages" />
+            <SectionButton text="Projects" />
+         </div>
+      </div>
+   );
+};
+
+export default MainContainer;
