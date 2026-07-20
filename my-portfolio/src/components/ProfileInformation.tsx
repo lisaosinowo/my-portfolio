bb6d0de2ccd4554b2f94eaadc6a731d46c06e806
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-20 12:57:16 +0100

    Added comments to differenciate the header sections

--- my-portfolio/src/components/ProfileInformation.tsx
+++ my-portfolio/src/components/ProfileInformation.tsx
@@ -14,11 +14,13 @@ const ProfileInformation = () => {
             <h1 className="text-3xl font-extrabold text-white">Lisa Osinowo</h1>
             <img src={check} className="w-7 h-7 ml-[7px] flex self-center" />
          </div>
+         
          {/* Username */}
          <p className="text-[--grey] text-base pb-[4px]">@lisaosinowo</p>
          <p className="flex inline-block whitespace-nowrap text-white text-sm w-[450px] pb-[4px]">"The best way to predict the future is to create it." -Alan Kay <img src={codeIMG} className="w-5 h-5 ml-[7px] " /></p>
          <p className="text-white text-sm pb-[4px]">Fullstack Developer. Quality Assurance Engineer.</p>
 
+         {/* The Profile Details */}
          <div className="flex gap-7 smm:gap-9 sm:gap-6 md:gap-3 pb-[4px]">
             <Detail src={briefcaseImg} text="Available" />
             <Detail src={linkImg} text="Links" />
