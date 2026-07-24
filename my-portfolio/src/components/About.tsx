b51c826c8a5dc17dc72adb4f1c146ace5cdf1328
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-24 12:58:41 +0100

    Created the About component

---
+++ my-portfolio/src/components/About.tsx
@@ -0,0 +1,39 @@
+const About = () => {
+   return (
+      <div className="center py-6">
+         <div className="w-[89%]">
+            <h1 className="text-3xl font-bold text-[--white] mb-6">
+               Hello World! 😊
+            </h1>
+            <p className="mb-3">
+               I am Lisa, a Software Development Engineer in Test, a Fullstack
+               Developer, and an Automation Engineer. Here's a little backstory about me and how I got into coding.
+            </p>
+
+            <p className="mb-3">
+               My Chemical Engineering degree is where my problem solving and
+               innovation skills stemmed from. After obtaining my degree, I found interest in coding thanks to my tech family. I
+               decided to transition into the tech world where I initially
+               taught myself how to code with Python.
+            </p>
+
+            <p className="mb-3">
+               In October 2024, I obtained a Frontend Development Fundamentals
+               certificate after completing my Frontend Development bootcamp with Black Girls in Tech.
+               Within the bootcamp, I learned HTML and CSS. I also learned
+               JavaScript and React after the bootcamp to expand my skillset.
+            </p>
+
+            <p className="mb-3">
+               In January 2025, I started a 16-week CFGdegree in Fullstack
+               Development with a +Masters in Cyber Security with Code First
+               Girls. This course included HTML, CSS, JavaScript, React,
+               TypeScript, Angular, and SQL. Thanks to Code First Girls, I was able to secure my first tech role at Capgemini as a Software Development Engineer in Test. 
+            </p>
+
+         </div>
+      </div>
+   );
+};
+
+export default About;
