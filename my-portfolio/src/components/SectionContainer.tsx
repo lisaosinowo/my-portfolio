08d7fda32ee91888f308cf7d2f0e79d22ce4c0e0
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-24 12:21:33 +0100

    Added the section container utility class to the SectionContainer component

--- my-portfolio/src/components/SectionContainer.tsx
+++ my-portfolio/src/components/SectionContainer.tsx
@@ -2,8 +2,10 @@ import Feed from "./Feed"
 
 const SectionContainer = () => {
   return (
-    <div className="border border-[var(--dark-navy-purple3)] border-4 rounded text-white main-container-width cursor-pointer">
-      <Feed />
+    <div>
+      <div className="section-container">
+        <Feed />
+      </div>
     </div>
   )
 }
