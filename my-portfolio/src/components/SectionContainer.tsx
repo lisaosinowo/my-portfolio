3132c510594033c111a3141c4c203d9f88b0e2b3
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-24 12:59:02 +0100

    Added the About component to the SectionContainer component

--- my-portfolio/src/components/SectionContainer.tsx
+++ my-portfolio/src/components/SectionContainer.tsx
@@ -1,10 +1,14 @@
 import Feed from "./Feed"
+import About from "./About"
 
 const SectionContainer = () => {
   return (
     <div>
       <div className="section-container">
         <Feed />
+      </div>
+      <div className="section-container">
+        <About />
       </div>
     </div>
   )
