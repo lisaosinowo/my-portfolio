9bff467e82436f9a01df9ceaf355015fb46d9b60
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-27 18:08:52 +0100

    Added min breakpoint of 768px, added overflow-x-hidden to the MainContainer div

--- my-portfolio/src/components/MainContainer.tsx
+++ my-portfolio/src/components/MainContainer.tsx
@@ -4,14 +4,14 @@ import SectionContainer from "./SectionContainer";
 
 const MainContainer = () => {
    return (
-      <div className="">
+      <div className="overflow-x-hidden">
          <div className="relative center">
             <div className="absolute top-[6.5rem] flex main-container-width lg:top-24 md:top-32 sm:top-32">
                <ProfileInformation />
             </div>
          </div>
 
-         <div className="flex main-container-width mt-[16.5rem] sm:mt-[18rem] md:mt-[18rem] lg:mt-[16rem] justify-around">
+         <div className="flex main-container-width mt-[16.5rem] sm:mt-[18rem] md:mt-[18rem] min-[768px]:mt-[19rem] lg:mt-[16rem] min-[1024px]:mt-[17rem] justify-around">
             <SectionButton text="Feed" />
             <SectionButton text="About" />
             <SectionButton text="Languages" />
