56b75d65714ca3631e64007ad50041bf2fbb1eb0
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-29 17:52:54 +0100

    Created a Project component for each individual project

---
+++ my-portfolio/src/components/Project.tsx
@@ -0,0 +1,14 @@
+interface ProjectProps {
+   name: string,
+   projectURL: string
+}
+
+const Project = ({ name, projectURL }: ProjectProps) => {
+   return (
+      <div onClick={() => window.open(`${projectURL}`, "_blank", "noopener,noreferrer")} className=" w-[89%] border border-[--navy-purple] hover:border-[--lilac] border-4 bg-[--dark-navy-purple3] hover:bg-[--purple] transition-colors duration-300 delay-250 center p-2 rounded-md mb-5">
+         <p className="text-sm min-[768px]:text-xl bg-transparent">{name}</p>
+      </div>
+   );
+};
+
+export default Project;
