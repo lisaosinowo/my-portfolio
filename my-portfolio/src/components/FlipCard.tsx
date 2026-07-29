ace3a25c1f42f6ea76b6a051590e11c0d8c29029
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-29 16:38:20 +0100

    Added a name p tag to each card, added max-[1100px]:flex col to the FlipCard parent div

--- my-portfolio/src/components/FlipCard.tsx
+++ my-portfolio/src/components/FlipCard.tsx
@@ -14,26 +14,31 @@ const FlipCard = ({ img, name, bgColor, textColor }: FlipCardProps) => {
    const [flipped, setFlipped] = useState(false);
 
    return (
-       <div
-      className={`
-        w-full h-28 max-[768px]:h-24 max-[530px]:h-20 max-[530px]:h-16
-        flex items-center justify-center
-        transition-transform duration-300
-        ${flipped ? "[transform:rotateY(180deg)]" : ""}
-      `}
-      onMouseEnter={() => setFlipped(true)}
-      onMouseLeave={() => setFlipped(false)}
-    >
-      {!flipped ? (
-        <Language img={img} />
-      ) : (
-        <LanguageName
-          name={name}
-          bgColor={bgColor}
-          textColor={textColor}
-        />
-      )}
-    </div>
+      <div className="">
+         <div
+            className={`
+          w-full h-28 max-[768px]:h-24 max-[530px]:h-20 max-[530px]:h-16
+          flex items-center justify-center
+          transition-transform duration-300 max-[1100px]:flex-col
+          ${flipped ? "[transform:rotateY(180deg)]" : ""}
+        `}
+            onMouseEnter={() => setFlipped(true)}
+            onMouseLeave={() => setFlipped(false)}
+         >
+            {!flipped ? (
+               <Language img={img} />
+            ) : (
+               <LanguageName
+                  name={name}
+                  bgColor={bgColor}
+                  textColor={textColor}
+               />
+            )}
+            <p className="min-[1025px]:hidden text-sm max-[376px]:text-xs min-[768px]:text-xl max-[301px]:mt-0 max-[430px]:mt-1 min-[768px]:mt-2">
+               {name}
+            </p>
+         </div>
+      </div>
    );
 };
 
