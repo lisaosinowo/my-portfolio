62702b4514ecb69a53f4ac2d4cf077e43ca9a1eb
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-29 18:15:49 +0100

    Centered the Project components horizontally

--- my-portfolio/src/components/Projects.tsx
+++ my-portfolio/src/components/Projects.tsx
@@ -11,20 +11,26 @@ const Projects = () => {
             <p className="text-sm min-[768px]:text-xl mb-4">
                Click below to view my Github Profile:
             </p>
-            <Project name={"GitHub Profile"} projectURL={"https://github.com/lisaosinowo"}/>
+            <div className="center">
+               <Project name={"GitHub Profile"} projectURL={"https://github.com/lisaosinowo"}/>
+            </div>
+            
 
             {/* Favourite GitHub Projects */}
             <p className="text-sm min-[768px]:text-xl mb-6">
                Here are some of my favourite projects! Click on one to view on Github:
             </p>
-            <Project name={"Nike E-commerce Site"} projectURL={"https://github.com/lisaosinowo/E-commerce-site-nike"} />
-            <Project name={"Nail Appointment RESTful API"} projectURL={"https://github.com/lisaosinowo/Nail-appointment-API"} />
-            <Project name={"Roleplay Game"} projectURL={"https://github.com/lisaosinowo/Roleplay-Game"} />
-            <Project name={"Snake Game"} projectURL={"https://github.com/lisaosinowo/Snake-game"} />
-            <Project name={"Turtle Race Game"} projectURL={"https://github.com/lisaosinowo/Turtle-race"} />
-            <Project name={"Quiz Game"} projectURL={"https://github.com/lisaosinowo/Quiz-game"} />
-            <Project name={"BlackJack Game"} projectURL={"https://github.com/lisaosinowo/BlackJack-game"} />
-            <Project name={"Hangman Game"} projectURL={"https://github.com/lisaosinowo/Hangman"} />
+            <div className="center items-center flex-col">
+               <Project name={"Nike E-commerce Site"} projectURL={"https://github.com/lisaosinowo/E-commerce-site-nike"} />
+               <Project name={"Nail Appointment RESTful API"} projectURL={"https://github.com/lisaosinowo/Nail-appointment-API"} />
+               <Project name={"Roleplay Game"} projectURL={"https://github.com/lisaosinowo/Roleplay-Game"} />
+               <Project name={"Snake Game"} projectURL={"https://github.com/lisaosinowo/Snake-game"} />
+               <Project name={"Turtle Race Game"} projectURL={"https://github.com/lisaosinowo/Turtle-race"} />
+               <Project name={"Quiz Game"} projectURL={"https://github.com/lisaosinowo/Quiz-game"} />
+               <Project name={"BlackJack Game"} projectURL={"https://github.com/lisaosinowo/BlackJack-game"} />
+               <Project name={"Hangman Game"} projectURL={"https://github.com/lisaosinowo/Hangman"} />
+            </div>
+            
          </div>
       </div>
    );
