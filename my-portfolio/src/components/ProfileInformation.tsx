8c78f0c75837af48224ff05048147eadc334fd2d
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-30 17:04:47 +0100

    Changed text size of the p tags

--- my-portfolio/src/components/ProfileInformation.tsx
+++ my-portfolio/src/components/ProfileInformation.tsx
@@ -16,14 +16,14 @@ const ProfileInformation = () => {
          
          {/* Username */}
          <p className="text-[--grey] min-[768px]:text-xl text-base pb-[4px]">@lisaosinowo</p>
-         <p className="flex inline-block whitespace-nowrap text-white min-[768px]:text-xl text-sm w-[450px] pb-[4px]">The best way to predict the future is to create it.<img src={codeIMG} className="max-[385px]:hidden w-5 h-5 ml-[7px] " /></p>
-         <p className="text-white whitespace-nowrap min-[768px]:text-xl text-sm pb-[4px]">Fullstack Developer. Certified Tester.</p>
+         <p className="flex inline-block whitespace-nowrap text-white min-[768px]:text-xl max-[375px]:text-sm text-base w-[450px] pb-[4px]">"Predict your future by creating it."<img src={codeIMG} className="w-5 h-5 ml-[7px] " /></p>
+         <p className="text-white whitespace-nowrap min-[768px]:text-xl max-[375px]:text-sm text-base pb-[4px]">Fullstack Developer. Certified Tester.</p>
 
          {/* The Profile Details */}
          <div className="flex gap-7 sm:gap-9 sm:gap-6 md:gap-3 pb-[4px]">
             <Detail src={briefcaseImg} text="Available" />
             <Detail src={linkImg} text="Links" />
-            <Detail src={calendarImg} text="Joined July 2026" />
+            <Detail src={calendarImg} text="Joined 07/26" />
          </div>
       </div>
    );
