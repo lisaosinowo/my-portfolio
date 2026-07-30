361c90b315725ece7801866929ebbd50eeae795f
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-30 17:05:00 +0100

    Changed text size of the p tags

--- my-portfolio/src/components/FeedPinned.tsx
+++ my-portfolio/src/components/FeedPinned.tsx
@@ -22,26 +22,26 @@ const FeedPinned = () => {
             </div>
 
             <div className="ml-[0.35rem] min-[768px]:mt-[-0.55rem]">
-               <p className="text-sm min-[768px]:text-xl mt-[0.15rem]">Pinned</p>
+               <p className="text-base min-[768px]:text-xl mt-[0.15rem]">Pinned</p>
                <div className="flex mt-2 mb-1">
                   <div className="flex">
                      {/* Pinned Details */}
-                     <p className="text-sm min-[768px]:text-xl mr-1">Lisa</p>
+                     <p className="text-base min-[768px]:text-xl  mr-1">Lisa</p>
                      <img
                         src={check}
-                        className="min-[768px]:w-[1.1rem] h-[1.1rem] w-[0.8rem] h-[0.8rem] mt-1 mr-[5px]"
+                        className="min-[768px]:w-[1.1rem] h-[1.1rem] w-[1.2rem] h-[0.8rem] mt-[0.2rem] min-[768px]:mt-[0.3rem] mr-[5px]"
                      />
                   </div>
-                  <p className="text-sm min-[768px]:text-xl mr-[5px] text-[--grey]">
+                  <p className="text-base min-[768px]:text-xl max-[375px]:text-sm max-[375px]:pt-[0.1rem] mr-[5px] text-[--grey]">
                      @lisaosinowo{" "}
                   </p>
-                  <p className="text-sm mr-[5px] text-[--grey] min-[768px]:text-xl">·</p>
-                  <p className="text-sm mr-[5px] text-[--grey] max-[358px]:text-xs min-[768px]:text-xl">20 July 2026</p>
+                  <p className="text-base mr-[5px] text-[--grey] min-[768px]:text-xl">·</p>
+                  <p className="text-base mr-[5px] text-[--grey] max-[358px]:text-xs max-[358px]:pt-[0.27rem] min-[768px]:text-xl max-[415px]:text-[0.85rem] ">20/07/2026</p>
                </div>
 
                {/* Pinned Text */}
                <div>
-                  <p className="text-sm min-[768px]:text-xl text-[--off-white]">
+                  <p className="text-base min-[768px]:text-xl max-[375px]:text-sm text-[--off-white]">
                      Welcome to my Portfolio 🤗 I had a lot of fun developing this site and I will be improving it when new ideas spawn. That's the life of a developer! 🫡 Use the four headers above this pinned post to explore!
                   </p>
                </div>
