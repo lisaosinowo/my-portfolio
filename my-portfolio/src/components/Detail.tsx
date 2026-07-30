3e7f6f09310af22bebb1056eed3893edcc92402b
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-07-30 17:05:21 +0100

    Changed text size of the p tags

--- my-portfolio/src/components/Detail.tsx
+++ my-portfolio/src/components/Detail.tsx
@@ -2,7 +2,7 @@ const Detail = ({src, text}: {src: string; text: string}) => {
   return (
     <div className="flex">
       <img src={src} className="min-[768px]:w-7 min-[768px]:h-7 w-5 h-5 mr-[5px]" />
-      <p className="text-[--grey] min-[768px]:text-xl text-sm whitespace-nowrap sm:mr-[15px]">{text}</p>
+      <p className="text-[--grey] min-[768px]:text-xl max-[375px]:text-sm text-base whitespace-nowrap sm:mr-[15px]">{text}</p>
     </div>
   )
 }
