7a7f88eb178aaa88a7dff8c6cb71ae717e91202e
Lisa Osinowo <liseozzy@hotmail.co.uk>
2026-09-01 15:14:40 +0100

    Changed the p tags to the AboutParagraph components

--- my-portfolio/src/components/About.tsx
+++ my-portfolio/src/components/About.tsx
@@ -1,3 +1,5 @@
+import AboutParagraph from "./AboutParagraph";
+
 const About = () => {
    return (
       <div className="center py-6">
@@ -5,32 +7,33 @@ const About = () => {
             <h1 className="text-2xl min-[768px]:text-3xl font-bold text-[--white] mb-6">
                Hello World! 😊
             </h1>
-            <p className="mb-3 text-base max-[358px]:text-[0.91rem] min-[768px]:text-xl">
-               I am Lisa, a Software Development Engineer in Test, a Fullstack
-               Developer, and an Automation Engineer. Here's a little backstory about me and how I got into coding.
-            </p>
+            <AboutParagraph
+               paragraph="I am Lisa, a Software Development Engineer in Test, a Fullstack
+               Developer, and an Automation Engineer. Here's a little backstory about me and how I got into coding."
+            />
 
-            <p className="mb-3 text-base max-[358px]:text-[0.91rem] min-[768px]:text-xl">
-               My Chemical Engineering degree is where my problem solving and
+            <AboutParagraph
+               paragraph="My Chemical Engineering degree is where my problem solving and
                innovation skills stemmed from. After obtaining my degree, I found interest in coding thanks to my tech family. I
                decided to transition into the tech world where I initially
-               taught myself how to code with Python.
-            </p>
+               taught myself how to code with Python."
+            />
 
-            <p className="mb-3 text-base max-[358px]:text-[0.91rem] min-[768px]:text-xl">
-               In October 2024, I obtained a Frontend Development Fundamentals
+            <AboutParagraph
+               paragraph="In October 2024, I obtained a Frontend Development Fundamentals
                certificate after completing my Frontend Development bootcamp with Black Girls in Tech.
                Within the bootcamp, I learned HTML and CSS. I also learned
-               JavaScript and React after the bootcamp to expand my skillset.
-            </p>
+               JavaScript and React after the bootcamp to expand my skillset."
+            />
 
-            <p className="mb-3 text-base max-[358px]:text-[0.91rem] min-[768px]:text-xl">
-               In January 2025, I started a 16-week CFGdegree in Fullstack
+            <AboutParagraph
+               paragraph="In January 2025, I started a 16-week CFGdegree in Fullstack
                Development with a +Masters in Cyber Security with Code First
                Girls. This course included HTML, CSS, JavaScript, React,
-               TypeScript, Angular, and SQL. Thanks to Code First Girls, I was able to secure my first tech role at Capgemini as a Software Development Engineer in Test. 
-            </p>
-
+               TypeScript, Angular, and SQL. Thanks to Code First Girls, I was
+               able to secure my first tech role at Capgemini as a Software
+               Development Engineer in Test."
+            />
          </div>
       </div>
    );
